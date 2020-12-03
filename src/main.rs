197e9ff15325d9702b5c9028fe2d2c9016e08a77
ptrkalm <pkalmuczak@gmail.com>
2020-12-03 15:33:18 +0100

    day 1-3

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod day1; mod day2; mod day3;
+
+fn main() {
+    println!("Day 1:");
+    println!("Part 1: {}", day1::part1(day1::INPUT));
+    println!("Part 2: {}", day1::part2(day1::INPUT));
+    println!("");
+    println!("Day 2:");
+    println!("Part 1: {}", day2::part1(day2::INPUT));
+    println!("Part 2: {}", day2::part2(day2::INPUT));
+    println!("");
+    println!("Day 3:");
+    println!("Part 1: {}", day3::part1(day3::INPUT));
+    println!("Part 2: {}", day3::part2(day3::INPUT));
+}
+
