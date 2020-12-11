8ab07b08ade025c68b1acfa15f9c162d13119c3e
ptrkalm <pkalmuczak@gmail.com>
2020-12-11 18:02:34 +0100

    day11

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@ macro_rules! solve {
     };
 }
 
-mod day1; mod day2; mod day3; mod day4; mod day5;
-mod day6; mod day7; mod day8; mod day9; mod day10;
+mod day1;  mod day2;  mod day3;  mod day4;  mod day5;
+mod day6;  mod day7;  mod day8;  mod day9;  mod day10;
+mod day11;
 
 fn main() {
     solve!(day1); solve!(day2); solve!(day3); solve!(day4); solve!(day5);
     solve!(day6); solve!(day7); solve!(day8); solve!(day9); solve!(day10);
+
+    solve!(day11);
 
 }
 
