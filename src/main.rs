fbbb5993c1c570ff0b9599bb4942e0f9dd3b8603
ptrkalm <pkalmuczak@gmail.com>
2020-12-13 14:53:03 +0100

    day12

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 macro_rules! solve {
     ($day:ident) => {
         println!(stringify!($day));
@@ -8,13 +9,11 @@ macro_rules! solve {
 
 mod day1;  mod day2;  mod day3;  mod day4;  mod day5;
 mod day6;  mod day7;  mod day8;  mod day9;  mod day10;
-mod day11;
+mod day11; mod day12;
 
 fn main() {
-    solve!(day1); solve!(day2); solve!(day3); solve!(day4); solve!(day5);
-    solve!(day6); solve!(day7); solve!(day8); solve!(day9); solve!(day10);
-
-    solve!(day11);
-
+   solve!(day1);  solve!(day2); solve!(day3); solve!(day4); solve!(day5);
+   solve!(day6);  solve!(day7); solve!(day8); solve!(day9); solve!(day10);
+   solve!(day11); solve!(day12);
 }
 
